d9aca813fcf4ef898398de310cc7ee42566f464b
Prabarshana <prabarshana@gmail.com>
2022-01-11 19:07:59 +0530

    initial commit

---
+++ src/main/java/com/isanka/product/SecureProductApplication.java
@@ -0,0 +1,13 @@
+package com.isanka.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isanka/product/SecureProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isanka.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
